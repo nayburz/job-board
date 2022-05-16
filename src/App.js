b01e76ace7765c061dbc90ee0789d8a917c4a022
Adrian Neighbors <adrian102@gmail.com>
2022-05-16 09:24:05 -0700

    Fetched data and added tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
+import React, { useState, useEffect } from "react";
+import data from "./assets/data.json";
+import JobBoardComponent from "./components/JobBoardComponent";
 
 function App() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    setJobs(data);
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello Adrian</h1>
+      {jobs.length === 0 ? (
+        <p>Jobs are fetching...</p>
+      ) : (
+        jobs.map((job) => <JobBoardComponent job={job} key={job.id} />)
+      )}
     </div>
   );
 }
---
+++ src/components/JobBoardComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const JobBoardComponent = () => {
+  return <div>Hi, I am a Job Board</div>;
+};
+
+export default JobBoardComponent;
