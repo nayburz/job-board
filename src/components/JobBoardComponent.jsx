99cbfc61f75d3c30538cfedfb7f1f40faa51b882
Adrian Neighbors <adrian102@gmail.com>
2022-05-19 12:13:22 -0700

    mobile fix

--- src/components/JobBoardComponent.jsx
+++ src/components/JobBoardComponent.jsx
@@ -30,21 +30,25 @@ const JobBoardComponent = ({
     <div
       className={`flex flex-col bg-white shadow-md my-16 mx-16 p-6 rounded ${
         featured && "border-l-4 border-teal-500 border-solid"
-      }`}
+      } sm:flex-row`}
     >
       <div>
-        <img className="-mt-16 mb-4 w-20 h-20" src={logo} alt={company} />
+        <img
+          className="-mt-16 mb-4 w-20 h-20 sm:h-24 sm:w-24 sm:my-0"
+          src={logo}
+          alt={company}
+        />
       </div>
       <div className="flex flex-col ml-4">
         <h3 className="font-bold text-teal-500">
           {company}
           {isNew && (
-            <span className="text-teal-100 bg-teal-500 font-bold m-2 py-1 px-2 rounded-full">
+            <span className="text-teal-100 bg-teal-500 font-bold m-2 py-1 px-2 rounded-full uppercase text-sm">
               New
             </span>
           )}
           {featured && (
-            <span className="text-white bg-gray-800 font-bold py-1 px-2 rounded-full">
+            <span className="text-white bg-gray-800 font-bold py-1 px-2 rounded-full uppercase text-sm">
               Featured
             </span>
           )}
@@ -54,10 +58,10 @@ const JobBoardComponent = ({
           {postedAt} · {contract} · {location}
         </p>
       </div>
-      <div className=" flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-solid">
+      <div className=" flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-solid sm:ml-auto sm:border-0 sm:pt-0 sm:mt-0 sm:items-center">
         {tags
           ? tags.map((tag) => (
-              <span className="text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded">
+              <span className="text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded sm:mb-0">
                 {tag}
               </span>
             ))
