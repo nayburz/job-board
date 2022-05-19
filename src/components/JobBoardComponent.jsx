04192fbc878fd02dbbe3eb7e22a7b23a2521a751
Adrian Neighbors <adrian102@gmail.com>
2022-05-19 10:53:00 -0700

    added mobile css

--- src/components/JobBoardComponent.jsx
+++ src/components/JobBoardComponent.jsx
@@ -27,9 +27,13 @@ const JobBoardComponent = ({
   }
 
   return (
-    <div className="flex bg-white shadow-md m-4 p-6 rounded">
+    <div
+      className={`flex flex-col bg-white shadow-md my-16 mx-16 p-6 rounded ${
+        featured && "border-l-4 border-teal-500 border-solid"
+      }`}
+    >
       <div>
-        <img src={logo} alt={company} />
+        <img className="-mt-16 mb-4 w-20 h-20" src={logo} alt={company} />
       </div>
       <div className="flex flex-col ml-4">
         <h3 className="font-bold text-teal-500">
@@ -45,15 +49,15 @@ const JobBoardComponent = ({
             </span>
           )}
         </h3>
-        <h2 className="font-bold text-xl">{position}</h2>
+        <h2 className="font-bold text-xl my-2">{position}</h2>
         <p className="text-gray-700">
           {postedAt} · {contract} · {location}
         </p>
       </div>
-      <div className=" flex ml-auto items-center justify-end">
+      <div className=" flex flex-wrap items-center mt-4 mx-4 pt-4 border-t border-gray-500 border-solid">
         {tags
           ? tags.map((tag) => (
-              <span className="text-teal-500 bg-teal-100 font-bold m-2 p-2 rounded-xl">
+              <span className="text-teal-500 bg-teal-100 font-bold mr-4 mb-4 p-2 rounded">
                 {tag}
               </span>
             ))
