187cfd8863fe32df3dc68c26735707eb0eee867f
Adrian Neighbors <adrian102@gmail.com>
2022-05-20 09:30:39 -0700

    Added clear function for filtered jobs

--- src/App.js
+++ src/App.js
@@ -38,6 +38,10 @@ function App() {
     setFilters(filters.filter((f) => f !== passedFilter));
   };
 
+  const clearFilters = () => {
+    setFilters([]);
+  };
+
   const filteredJobs = jobs.filter(filterFunc);
 
   return (
@@ -47,16 +51,24 @@ function App() {
       </header>
 
       {filters.length > 0 && (
-        <div className={`flex bg-white shadow-md my-16 mx-10 p-6 rounded`}>
-          {filters.map((filter) => (
-            <span
-              className="cursor-pointer mr-4 mb-4 rounded font-bold text-teal-500 bg-teal-100 p-2 sm:mb-0"
-              onClick={() => handleFilterClick(filter)}
+        <>
+          <div className={`flex bg-white shadow-md my-16 mx-10 p-6 rounded`}>
+            {filters.map((filter) => (
+              <span
+                className="cursor-pointer mr-4 mb-4 rounded font-bold text-teal-500 bg-teal-100 p-2 sm:mb-0"
+                onClick={() => handleFilterClick(filter)}
+              >
+                <span>x {filter}</span>
+              </span>
+            ))}
+            <button
+              onClick={clearFilters}
+              className="cursor-pointer font-bold text-gray-700 ml-auto"
             >
-              <span>x {filter}</span>
-            </span>
-          ))}
-        </div>
+              Clear
+            </button>
+          </div>
+        </>
       )}
       {jobs.length === 0 ? (
         <p>Jobs are fetching...</p>
